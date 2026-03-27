1ccff057729504004195d28575b0c795ae4aa889
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 15:23:40 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -2,25 +2,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class FrequencyDictionary {
+public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        if (scanner.hasNextLine()) {
-            String text = scanner.nextLine();
-            String[] words = text.split("\\s+");
+        String text = scanner.nextLine();
+        String[] words = text.split("\\s+");
 
-            HashMap<String, Integer> wordCounts = new HashMap<>();
+        HashMap<String, Integer> wordCounts = new HashMap<>();
 
-            for (String word : words) {
-                if (!word.isEmpty()) {
-                    wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
-                }
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
             }
+        }
 
-            for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
         scanner.close();
